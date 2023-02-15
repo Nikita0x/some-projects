91c3212eb15a33372287e41f0acddbfecaecf6d9
Nikita0x <114003900+Nikita0x@users.noreply.github.com>
2023-02-15 04:20:57 +0200

    video_test

--- cities/dist/newyork.js
+++ cities/dist/newyork.js
@@ -1,33 +1,45 @@
 // New York carousel
 
+// creating a variable with the value of 1
 let slideIndex_ny = 1;
+// on load, call future function with the index of 1
 showSlides_ny(slideIndex_ny);
 
-function plusSlides_ny(a) {
-  showSlides_ny((slideIndex_ny += a));
+// create a function (for next and previous buttons)
+function plusSlides_ny(n) {
+  showSlides_ny((slideIndex_ny += n));
 }
 
 // // Thumbnail image controls
-function currentSlide_ny(a) {
-  showSlides_ny((slideIndex_ny = a));
+function currentSlide_ny(n) {
+  showSlides_ny((slideIndex_ny = n));
 }
-
-function showSlides_ny(a) {
-  let b;
+// declaring a function
+function showSlides_ny(n) {
+  // targeting DOM objects and storing them in variables
+  let i;
   let slides = document.getElementsByClassName("mySlides_ny");
   let dots = document.getElementsByClassName("dot_ny");
-  if (a > slides.length) {
+
+  // if page is 5 or > 4, set index to 1
+  if (n > slides.length) {
     slideIndex_ny = 1;
   }
-  if (a < 1) {
+  // if page is 0 or <0, set index to 4(last element)
+  if (n < 1) {
     slideIndex_ny = slides.length;
   }
+
   // basic for loop
-  for (b = 0; b < slides.length; b++) {
-    slides[b].style.display = "none";
+
+  // this loop is supposed to run over all mySlides_ny and set their display to "none"
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  for (b = 0; b < dots.length; b++) {
-    dots[b].className = dots[b].className.replace(" active_ny", "");
+
+  // this loop is supposed to run over dots_ny and
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active_ny", "");
   }
   slides[slideIndex_ny - 1].style.display = "block";
   dots[slideIndex_ny - 1].className += " active_ny";
---
+++ video_test/main.js
@@ -0,0 +1,16 @@
+// get the video
+
+let video = document.getElementById("myVideo");
+
+// get button
+let btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
