896189165858a32ddf4985e544d5623f1d96ab58
Nikita0x <114003900+Nikita0x@users.noreply.github.com>
2023-02-20 15:25:41 +0200

    added  smooth transition

--- blizzard/main.js
+++ blizzard/main.js
@@ -1,17 +1,14 @@
 let wow_image = document.getElementById("wow_image");
 let wow_card = document.getElementById("wow_card");
-let wow_link = document.getElementById("test");
 let wow_preview = document.getElementById("wow_preview");
-let mouseHasEntered = false;
 
 wow_card.addEventListener("mouseenter", function () {
   wow_image.style.display = "none";
   wow_preview.style.display = "block";
-  mouseHasEntered = true;
 
   console.log(wow_image);
   console.log(wow_preview);
-  console.log("Image to preview -changed");
+  console.log("Image hidden, preview visible");
 });
 
 wow_card.addEventListener("mouseleave", function () {
@@ -19,9 +16,7 @@ wow_card.addEventListener("mouseleave", function () {
   wow_preview.style.display = "none";
   wow_preview.currentTime = 0;
 
-  mouseHasEntered = false;
-
   console.log(wow_image);
   console.log(wow_preview);
-  console.log("Image to preview to image");
+  console.log("Image visible, preview hidden");
 });
