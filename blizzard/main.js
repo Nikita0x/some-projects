23161eb6b78b09d2052a64bc003f9e98b8ebf337
Nikita0x <114003900+Nikita0x@users.noreply.github.com>
2023-02-21 04:06:44 +0200

    added video previews, fixed height

--- blizzard/main.js
+++ blizzard/main.js
@@ -1,3 +1,5 @@
+// wow js
+
 let wow_image = document.getElementById("wow_image");
 let wow_card = document.getElementById("wow_card");
 let wow_preview = document.getElementById("wow_preview");
@@ -20,3 +22,239 @@ wow_card.addEventListener("mouseleave", function () {
   console.log(wow_preview);
   console.log("Image visible, preview hidden");
 });
+
+// wow js
+
+// overwatch 2 js
+
+let overwatch2_image = document.getElementById("overwatch2_image");
+let overwatch2_card = document.getElementById("overwatch2_card");
+let overwatch2_preview = document.getElementById("overwatch2_preview");
+
+overwatch2_card.addEventListener("mouseenter", function () {
+  overwatch2_image.style.display = "none";
+  overwatch2_preview.style.display = "block";
+
+  console.log(overwatch2_image);
+  console.log(overwatch2_preview);
+  console.log("Image hidden, preview visible");
+});
+
+overwatch2_card.addEventListener("mouseleave", function () {
+  overwatch2_image.style.display = "block";
+  overwatch2_preview.style.display = "none";
+  overwatch2_preview.currentTime = 0;
+
+  console.log(overwatch2_image);
+  console.log(overwatch2_preview);
+  console.log("Image visible, preview hidden");
+});
+// overwatch 2 js
+
+// wow classic  js
+let wowclassic_image = document.getElementById("wowclassic_image");
+let wowclassic_card = document.getElementById("wowclassic_card");
+let wowclassic_preview = document.getElementById("wowclassic_preview");
+
+wowclassic_card.addEventListener("mouseenter", function () {
+  wowclassic_image.style.display = "none";
+  wowclassic_preview.style.display = "block";
+
+  console.log(wowclassic_image);
+  console.log(wowclassic_preview);
+  console.log("Image hidden, preview visible");
+});
+
+wowclassic_card.addEventListener("mouseleave", function () {
+  wowclassic_image.style.display = "block";
+  wowclassic_preview.style.display = "none";
+  wowclassic_preview.currentTime = 0;
+
+  console.log(wowclassic_image);
+  console.log(wowclassic_preview);
+  console.log("Image visible, preview hidden");
+});
+// wow classic  js
+
+// hearthstone js
+let hearthstone_image = document.getElementById("hearthstone_image");
+let hearthstone_card = document.getElementById("hearthstone_card");
+let hearthstone_preview = document.getElementById("hearthstone_preview");
+
+hearthstone_card.addEventListener("mouseenter", function () {
+  hearthstone_image.style.display = "none";
+  hearthstone_preview.style.display = "block";
+
+  console.log(hearthstone_image);
+  console.log(hearthstone_preview);
+  console.log("Image hidden, preview visible");
+});
+
+hearthstone_card.addEventListener("mouseleave", function () {
+  hearthstone_image.style.display = "block";
+  hearthstone_preview.style.display = "none";
+  hearthstone_preview.currentTime = 0;
+
+  console.log(hearthstone_image);
+  console.log(hearthstone_preview);
+  console.log("Image visible, preview hidden");
+});
+// hearthstone js
+
+// diablo immortal js
+let diabloimmortal_image = document.getElementById("diabloimmortal_image");
+let diabloimmortal_card = document.getElementById("diabloimmortal_card");
+let diabloimmortal_preview = document.getElementById("diabloimmortal_preview");
+
+diabloimmortal_card.addEventListener("mouseenter", function () {
+  diabloimmortal_image.style.display = "none";
+  diabloimmortal_preview.style.display = "block";
+
+  console.log(diabloimmortal_image);
+  console.log(diabloimmortal_preview);
+  console.log("Image hidden, preview visible");
+});
+
+diabloimmortal_card.addEventListener("mouseleave", function () {
+  diabloimmortal_image.style.display = "block";
+  diabloimmortal_preview.style.display = "none";
+  diabloimmortal_preview.currentTime = 0;
+
+  console.log(diabloimmortal_image);
+  console.log(diabloimmortal_preview);
+  console.log("Image visible, preview hidden");
+});
+// diablo immortal js
+
+// diablo 2 resurrected js
+let diablo2ressurected_image = document.getElementById(
+  "diablo2ressurected_image"
+);
+let diablo2ressurected_card = document.getElementById(
+  "diablo2ressurected_card"
+);
+let diablo2ressurected_preview = document.getElementById(
+  "diablo2ressurected_preview"
+);
+
+diablo2ressurected_card.addEventListener("mouseenter", function () {
+  diablo2ressurected_image.style.display = "none";
+  diablo2ressurected_preview.style.display = "block";
+
+  console.log(diablo2ressurected_image);
+  console.log(diablo2ressurected_preview);
+  console.log("Image hidden, preview visible");
+});
+
+diablo2ressurected_card.addEventListener("mouseleave", function () {
+  diablo2ressurected_image.style.display = "block";
+  diablo2ressurected_preview.style.display = "none";
+  diablo2ressurected_preview.currentTime = 0;
+
+  console.log(diablo2ressurected_image);
+  console.log(diablo2ressurected_preview);
+  console.log("Image visible, preview hidden");
+});
+// diablo 2 resurrected js
+
+// diablo 3 js
+let diablo3_image = document.getElementById("diablo3_image");
+let diablo3_card = document.getElementById("diablo3_card");
+let diablo3_preview = document.getElementById("diablo3_preview");
+
+diablo3_card.addEventListener("mouseenter", function () {
+  diablo3_image.style.display = "none";
+  diablo3_preview.style.display = "block";
+
+  console.log(diablo3_image);
+  console.log(diablo3_preview);
+  console.log("Image hidden, preview visible");
+});
+
+diablo3_card.addEventListener("mouseleave", function () {
+  diablo3_image.style.display = "block";
+  diablo3_preview.style.display = "none";
+  diablo3_preview.currentTime = 0;
+
+  console.log(diablo3_image);
+  console.log(diablo3_preview);
+  console.log("Image visible, preview hidden");
+});
+// diablo 3 js
+
+// starcraft 2 js
+let starcraft2_image = document.getElementById("starcraft2_image");
+let starcraft2_card = document.getElementById("starcraft2_card");
+let starcraft2_preview = document.getElementById("starcraft2_preview");
+
+starcraft2_card.addEventListener("mouseenter", function () {
+  starcraft2_image.style.display = "none";
+  starcraft2_preview.style.display = "block";
+
+  console.log(starcraft2_image);
+  console.log(starcraft2_preview);
+  console.log("Image hidden, preview visible");
+});
+
+starcraft2_card.addEventListener("mouseleave", function () {
+  starcraft2_image.style.display = "block";
+  starcraft2_preview.style.display = "none";
+  starcraft2_preview.currentTime = 0;
+
+  console.log(starcraft2_image);
+  console.log(starcraft2_preview);
+  console.log("Image visible, preview hidden");
+});
+// starcraft 2 js
+
+// warcraft 3 reforged js
+let warcraft3_image = document.getElementById("warcraft3_image");
+let warcraft3_card = document.getElementById("warcraft3_card");
+let warcraft3_preview = document.getElementById("warcraft3_preview");
+
+warcraft3_card.addEventListener("mouseenter", function () {
+  warcraft3_image.style.display = "none";
+  warcraft3_preview.style.display = "block";
+
+  console.log(warcraft3_image);
+  console.log(warcraft3_preview);
+  console.log("Image hidden, preview visible");
+});
+
+warcraft3_card.addEventListener("mouseleave", function () {
+  warcraft3_image.style.display = "block";
+  warcraft3_preview.style.display = "none";
+  warcraft3_preview.currentTime = 0;
+
+  console.log(warcraft3_image);
+  console.log(warcraft3_preview);
+  console.log("Image visible, preview hidden");
+});
+// warcraft 3 reforged js
+
+// hears of the strom js
+let heroesofthestorm_image = document.getElementById("heroesofthestorm_image");
+let heroesofthestorm_card = document.getElementById("heroesofthestorm_card");
+let heroesofthestorm_preview = document.getElementById(
+  "heroesofthestorm_preview"
+);
+
+heroesofthestorm_card.addEventListener("mouseenter", function () {
+  heroesofthestorm_image.style.display = "none";
+  heroesofthestorm_preview.style.display = "block";
+
+  console.log(heroesofthestorm_image);
+  console.log(heroesofthestorm_preview);
+  console.log("Image hidden, preview visible");
+});
+
+heroesofthestorm_card.addEventListener("mouseleave", function () {
+  heroesofthestorm_image.style.display = "block";
+  heroesofthestorm_preview.style.display = "none";
+  heroesofthestorm_preview.currentTime = 0;
+
+  console.log(heroesofthestorm_image);
+  console.log(heroesofthestorm_preview);
+  console.log("Image visible, preview hidden");
+});
+// hears of the strom js
