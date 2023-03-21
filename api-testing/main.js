9ecfaff9a37a8f717c707ffccf3e0a1c8a0aa29b
Nikita0x <114003900+Nikita0x@users.noreply.github.com>
2023-03-21 02:21:34 +0200

    gwag
    gwa

--- api-testing/main.js
+++ api-testing/main.js
@@ -1,19 +1,81 @@
 const button = document.querySelector(".btn");
-const image = document.querySelector(".image");
-const url = "https://aws.random.cat/meow";
-const divSource = document.querySelector("#source");
+let image = document.querySelector(".image");
+let image2 = document.querySelector(".image2");
+let image3 = document.querySelector(".image3");
 
-async function fetchHandler() {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    image.src = data.file;
-    console.log(image.src);
-  } catch (error) {
-    console.log(error);
-  }
+const url = "https://aws.random.cat/meow";
+const cachedImage = document.querySelector("#cachedImage")
+const buttonTwo = document.querySelector(".button")
+let data1 = [];
+let data2 = [];
+let data3 = [];
+
+let clicksCount = 0;
+
+
+function fetchImage1() {
+
+  fetch(url)
+  .then(response => response.json())
+  .then(json => {
+    data1 = json;
+    console.log(data1);
+    image.src = data1.file;
+  })
+  .catch(err => console.error(err));
 }
 
+function fetchImage2() {
+  fetch(url)
+  .then(response => response.json())
+  .then(json => {
+    data2 = json;
+    console.log(data2);
+    image2.src = data2.file;
+  })
+  .catch(err => console.error(err));
+}
+
+function fetchImage3(){
+  fetch(url)
+  .then(response => response.json())
+  .then(json => {
+    data3 = json;
+    console.log(data3);
+    image3.src = data3.file;
+  })
+  .catch(err => console.error(err));
+}
+// upon load of the page
+
+// button click listener
 button.addEventListener("click", () => {
-  fetchHandler();
+  clicksCount++;
+  if (clicksCount == 1) {
+    fetchImage1();
+    fetchImage2()
+    fetchImage3()
+    image.style.display = 'block';
+    image2.style.display = 'none';
+    image3.style.display = 'none';
+  } else if (clicksCount == 2) {
+    image.style.display = 'none';
+    image2.style.display = 'block';
+    image3.style.display = 'none';
+  } else if (clicksCount == 3) {
+    image.style.display = 'none';
+    image2.style.display = 'none';
+    image3.style.display = 'block';
+  }
+    else {
+      clicksCount = 1;
+      fetchImage1();
+      fetchImage2()
+      fetchImage3()
+      image.style.display = 'block';
+      image2.style.display = 'none';
+      image3.style.display = 'none';
+    }
+    console.log(clicksCount);
 });
+
